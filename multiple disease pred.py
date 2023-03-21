a04e13a92b8fa5a522189699a721d57026f0b820
Parth Adokar <adokar.parth@gmail.com>
2023-03-21 14:59:24 +0530

    first

--- multiple disease pred.py
+++ multiple disease pred.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May 22 11:53:51 2022
-
-@author: siddhardhan
-"""
 
 import pickle
 import streamlit as st
@@ -12,11 +6,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('D:/Major Project/Multiple Disease Prediction System/saved models/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('D:\Sem-7\Major Project\Multiple Disease Prediction System\saved models/diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('D:/Major Project/Multiple Disease Prediction System/saved models/heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('D:\Sem-7\Major Project\Multiple Disease Prediction System\saved models/heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('D:/Major Project/Desktop/Multiple Disease Prediction System/saved models/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('D:\Sem-7\Major Project\Multiple Disease Prediction System\saved models/parkinsons_model.sav', 'rb'))
 
 
 
